9e0584710fe679102886a3bba1e68fc843e3fcd7
Scharly <scharly.opitz@gmail.com>
2025-02-20 11:26:23 +0100

    Ajout du rendu dans le canvas et changement de taille

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import * as THREE from "three";
 
+const canvas = document.querySelector(".webgl");
+
 // Scene
 const scene = new THREE.Scene();
 
@@ -15,3 +17,11 @@ const size = { width: 800, height: 600 };
 
 const camera = new THREE.PerspectiveCamera(75, size.width / size.height);
 scene.add(camera);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+
+renderer.setSize(size.width, size.height);
+
+renderer.render(scene, camera);
