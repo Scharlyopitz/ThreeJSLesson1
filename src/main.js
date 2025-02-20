2fedb2327f0d1a2e8f4a6afd788ad05561d8b9d1
Scharly <scharly.opitz@gmail.com>
2025-02-20 11:08:30 +0100

    Ajout scene et objets

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
+import * as THREE from "three";
 
+// Scene
+const scene = new THREE.Scene();
+
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: "#000000" });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// Camera
