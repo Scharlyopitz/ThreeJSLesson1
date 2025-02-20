03923f8e234db4e6d87e2c2b91650f7d4ec78e81
Scharly <scharly.opitz@gmail.com>
2025-02-20 11:18:28 +0100

    Ajout des paramètres de la caméra et du canvas dans le html

--- src/main.js
+++ src/main.js
@@ -10,3 +10,8 @@ const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
 // Camera
+
+const size = { width: 800, height: 600 };
+
+const camera = new THREE.PerspectiveCamera(75, size.width / size.height);
+scene.add(camera);
