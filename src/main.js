eed80c68cd85fbde8d871d8187456ee30a551fa0
Scharly <scharly.opitz@gmail.com>
2025-02-21 12:37:58 +0100

    Animation mouvement de la caméra au déplacement de la souris

--- src/main.js
+++ src/main.js
@@ -1,7 +1,21 @@
+import { animate } from "motion";
 import "./style.css";
 import * as THREE from "three";
 
 const canvas = document.querySelector(".webgl");
+
+// Cursor
+
+const cursor = {
+  x: 0,
+  y: 0,
+};
+
+const mouseMoove = (e) => {
+  cursor.x = e.clientX / size.width - 0.5;
+  cursor.y = e.clientY / size.height - 0.5;
+};
+window.addEventListener("mousemove", mouseMoove);
 
 // Scene
 const scene = new THREE.Scene();
@@ -17,12 +31,13 @@ const numberRotation = (number) => {
   return number * (Math.PI / 180);
 };
 
-const animateCube = (e) => {
-  cube.rotation.x = numberRotation(e.clientY);
-  cube.rotation.y = numberRotation(e.clientX);
-};
-
-window.addEventListener("mousemove", animateCube);
+// animate(
+//   cube.rotation,
+//   {
+//     y: numberRotation(360),
+//   },
+//   { duration: 10, repeat: Infinity, ease: "linear" }
+// );
 
 scene.add(cube);
 
@@ -32,7 +47,17 @@ const size = { width: window.innerWidth, height: window.innerHeight };
 
 const camera = new THREE.PerspectiveCamera(75, size.width / size.height);
 
-camera.position.set(0, 0, 3);
+// const aspectRatio = size.width / size.height;
+// const camera = new THREE.OrthographicCamera(
+//   -1 * aspectRatio,
+//   1 * aspectRatio,
+//   1,
+//   -1,
+//   0.1,
+//   100
+// );
+
+camera.position.set(0, 0, 2);
 scene.add(camera);
 
 // LookAt
@@ -51,6 +76,13 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 const tick = () => {
   // Render
   renderer.render(scene, camera);
+
+  // Update camera
+
+  camera.position.x = Math.sin(-cursor.x * Math.PI * 2) * 3;
+  camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
+  camera.position.y = cursor.y * 3;
+  camera.lookAt(cube.position);
 
   // Animation(activation)
   window.requestAnimationFrame(tick);
